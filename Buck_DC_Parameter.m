983ce20f0da30d4b1c328222f90b12eee7ebf871
tungngt <ngtntung@gmail.com>
2020-11-10 11:02:02 +0700

    Initial commit

---
+++ Buck_DC_Parameter.m
@@ -0,0 +1,32 @@
+clear;
+T = 0.01;
+%% DCM Model 
+    Udm = 220;          Idm = 21;
+    ndm = 1000;         wdm = ndm/9.55;
+    Ru = 0.89;          Lu = 0.05;
+    J = 0.05;           Pdm = 3700;
+    Edm = Udm-Ru*Idm;   
+    Kphi = Edm/wdm;
+%% DC Controller
+    % Speed Controller
+        Tres_speed = 0.1;           
+        Kp_W = 3*J/Tres_speed;
+    % Current Controller
+        Tres_current = 0.01;
+        Ki_I = 3*Ru/Tres_current;   
+        Kp_I = Ki_I*(Lu/Ru);
+%% Buck Model
+    Ubat = 400;
+    L = 4.7e-3;         rL = 0.005;
+    C = 4700e-6;
+%% Buck Controller
+    % Uc Controller
+        Tres_Uc = 0.04;         
+        Kp_Uc = 3*C/Tres_Uc;
+    % IL Controller9
+        Tres_IL = 0.004;
+        Ki_IL = 3*rL/Tres_IL;   
+        Kp_IL = Ki_IL*(L/rL);
+%% Max
+    ILmax = 60;         Ucmax = Ubat;
+    Iumax = 3*Idm;      wmax = 150;
